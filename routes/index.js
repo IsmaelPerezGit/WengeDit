af8439afd2d814714a73bea5252a964f811844f7
ismael perez <ismaelperezemail@gmail.com>
2017-08-17 19:34:56 -0700

    created .gitignore for node_modules. created create_user view and view for showing selected post

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,36 @@ var knex = require('../db/knex.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WengeDit' });
+  knex.raw(`select * from postinfo`)
+    .then(function(data) {
+      res.render('index', {
+        post: data.rows,
+        title: 'WengeDit'
+      });
+      console.log(data.rows[0].name);
+    })
+});
+
+//get individual story
+router.get('/post/:id', function(req, res, next) {
+  knex.raw(`select postinfo.content from postinfo where postinfo.id = ${req.params.id}`)
+    .then(function(data) {
+      res.render('individualPost', {
+        postTitle: data.rows,
+        title: 'WengeDit',
+        storyTitle: data.rows[0].content,
+        id: parseInt(req.params.id)
+      });
+      console.log(data.rows[0].content);
+    })
+});
+
+router.get('/new_user', function(req, res, next) {
+  res.render('new_user')
+});
+
+router.post('/new_user', function(req, res, next) {
+  if (req.body.res === req.body.confirm){} 
 });
 
 module.exports = router;
--- seeds/commentinfo.js
+++ seeds/commentinfo.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('commentinfo').insert([
         {content:'Make a personal website', user_id:1, post_id:1},
-        {content:'The elephant is a nice ', user_id:2, post_id:3},
+        {content:'The elephant is a nice elephant ', user_id:2, post_id:3},
         {content:'Destiny 2', user_id:3, post_id:2}
       ]);
     });
